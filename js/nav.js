db9cafd563fa1011b4c554f3d2df49c3eb9128df
Dominik Szafara <dominikszafara@gmail.com>
2022-12-19 11:31:36 +0100

    working with logo

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,7 @@
 const open = document.querySelector(".mob-open");
 const close = document.querySelector(".mob-close");
 
-const wrapper = document.querySelector(".mob-wrapper");
+const wrapper = document.querySelector(".mob-wrapper").classList;
 
 open.addEventListener("click", () => {
     wrapper.toggle("opened");
